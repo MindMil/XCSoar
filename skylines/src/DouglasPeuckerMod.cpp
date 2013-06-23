fab0c6fdbe8800d60228fecedc6e4833b2331e31
Tobias Lohner <tobias@lohner-net.de>
2013-06-23 19:04:24 +0200

    DouglasPeuckerMod: adjust coding style

--- skylines/src/DouglasPeuckerMod.cpp
+++ skylines/src/DouglasPeuckerMod.cpp
@@ -1,90 +1,123 @@
+/* Copyright_License {
+
+  XCSoar Glide Computer - http://www.xcsoar.org/
+  Copyright (C) 2000-2013 The XCSoar Project
+  A detailed list of copyright holders can be found in the file "AUTHORS".
+
+  This program is free software; you can redistribute it and/or
+  modify it under the terms of the GNU General Public License
+  as published by the Free Software Foundation; either version 2
+  of the License, or (at your option) any later version.
+
+  This program is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License
+  along with this program; if not, write to the Free Software
+  Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+}
+
+  Original code of Douglas-Peucker algorithm by Robert Coup <robert.coup@koordinates.com>
+*/
+
 #include "DouglasPeuckerMod.hpp"
 
 #include <stack>
-#include <sstream>
+#include <vector>
+#include <list>
+#include <utility>
+#include <algorithm>
 
-#include <stdio.h>
+DouglasPeuckerMod::DouglasPeuckerMod(int _num_levels,
+                                     int _zoom_factor,
+                                     double _threshold,
+                                     bool _force_endpoints)
+  : num_levels(_num_levels), zoom_factor(_zoom_factor),
+    threshold(_threshold), force_endpoints(_force_endpoints) {
+  zoom_level_breaks = new double[num_levels];
 
-using namespace std;
-
-DouglasPeuckerMod::DouglasPeuckerMod(int numLevels, int zoomFactor, double threshold, bool forceEndpoints)
-    : numLevels(numLevels), zoomFactor(zoomFactor), threshold(threshold), forceEndpoints(forceEndpoints)
-    {
-        zoomLevelBreaks = new double[numLevels];
-        for (int i=0; i<numLevels; i++) {
-            zoomLevelBreaks[i] = threshold * pow((double)zoomFactor, numLevels - i - 1);
-        }
-    }
-
-DouglasPeuckerMod::~DouglasPeuckerMod() {
-    delete[] zoomLevelBreaks;
+  for (int i = 0; i < num_levels; i++) {
+    zoom_level_breaks[i] = threshold * pow(double(zoom_factor), num_levels - i - 1);
+  }
 }
 
-vector<int> DouglasPeuckerMod::dpEncode(vector<vector<double> >& points, char *type) {
-    unsigned i, maxLoc = 0;
-    stack<pair<int, int> > stack;
-    double *dists = new double[points.size()];
-    fill(&dists[0], &dists[points.size()], 0.0);
-    double temp, maxDist;
+DouglasPeuckerMod::~DouglasPeuckerMod() {
+  delete[] zoom_level_breaks;
+}
 
-    double absMaxDist_squared = 0.0, absMaxDist;
-    double threshold_squared = pow(threshold, 2);
+std::vector<int> DouglasPeuckerMod::dpEncode(std::vector<std::vector<double>> &points, char *type) {
+  unsigned i,
+           max_loc = 0;
+  std::stack<std::pair<int, int>> stack;
 
+  double *dists = new double[points.size()];
+  fill(&dists[0], &dists[points.size()], 0.0);
 
+  double temp,
+         max_dist,
+         abs_max_dist_squared = 0.0,
+         abs_max_dist,
+         threshold_squared = pow(threshold, 2);
 
-    // use normal douglas peucker distance (perpendicular to segment)
-    // or use simple distance calculation from adjacent points
-    list<size_t> points_dp, points_simple;
+  /**
+   * use normal douglas peucker distance (perpendicular to segment)
+   * or use simple distance calculation from adjacent points
+   */
+  std::list<size_t> points_dp, points_simple;
 
-    for (i = 0; i < sizeof(type)/sizeof(type[0]); i++) {
-      if (type[i] == 'd')
-        points_simple.push_back(i);
-      else if (type[i] == 'p')
-        points_dp.push_back(i);
-      else
-        break;
-    }
-
-    for (i; i < points[0].size(); i++)
+  for (i = 0; i < sizeof(type) / sizeof(type[0]); i++) {
+    if (type[i] == 'd')
+      points_simple.push_back(i);
+    else if (type[i] == 'p')
       points_dp.push_back(i);
+    else
+      break;
+  }
 
-    // simplify using Douglas-Peucker
-    if (points.size() > 2) {
-        stack.push(pair<unsigned, unsigned>(0, (points.size() - 1)));
+  for (i; i < points[0].size(); i++)
+    points_dp.push_back(i);
 
-        while (stack.size() > 0) {
-            pair<int, int> current = stack.top();
-            stack.pop();
-            maxDist = 0;
+  // simplify using Douglas-Peucker
+  if (points.size() > 2) {
+    stack.push(std::pair<unsigned, unsigned>(0, (points.size() - 1)));
 
-            for (int i = current.first + 1; i < current.second; i++) {
-                temp = max(distance_dp(points[i], points[current.first], points[current.second], points_dp),
-                  distance_simple(points[i], points[current.first], points[current.second], points_simple));
+    while (stack.size() > 0) {
+      std::pair<int, int> current = stack.top();
+      stack.pop();
+      max_dist = 0;
 
-                if (temp > maxDist) {
-                    maxDist = temp;
-                    maxLoc = i;
-                }
+      for (int i = current.first + 1; i < current.second; i++) {
+        temp = std::max(distance_dp(points[i], points[current.first],
+                                    points[current.second], points_dp),
+                        distance_simple(points[i], points[current.first],
+                                        points[current.second], points_simple));
 
-            }
-
-            if (maxDist > absMaxDist_squared) {
-                absMaxDist_squared = maxDist;
-            }
-            if (maxDist > threshold_squared) {
-                dists[maxLoc] = sqrt(maxDist);
-                stack.push(pair<int, int>(current.first, maxLoc));
-                stack.push(pair<int, int>(maxLoc, current.second));
-            }
+        if (temp > max_dist) {
+          max_dist = temp;
+          max_loc = i;
         }
+      }
+
+      if (max_dist > abs_max_dist_squared) {
+        abs_max_dist_squared = max_dist;
+      }
+
+      if (max_dist > threshold_squared) {
+        dists[max_loc] = sqrt(max_dist);
+        stack.push(std::pair<int, int>(current.first, max_loc));
+        stack.push(std::pair<int, int>(max_loc, current.second));
+      }
     }
+  }
 
-    absMaxDist = sqrt(absMaxDist_squared);
+  abs_max_dist = sqrt(abs_max_dist_squared);
 
-    vector<int> r = classify(points.size(), dists, absMaxDist);
+  std::vector<int> r = classify(points.size(), dists, abs_max_dist);
 
-    delete[] dists;
-    return r;
+  delete[] dists;
+  return r;
 }
 
 /**
@@ -92,79 +125,90 @@ vector<int> DouglasPeuckerMod::dpEncode(vector<vector<double> >& points, char *t
  * segment [p1,p2]. This could probably be replaced with something that is a
  * bit more numerically stable.
  */
-double DouglasPeuckerMod::distance_dp(vector<double>& p0, vector<double>& p1, vector<double>& p2, list<size_t>& points) {
-    double u, out = 0.0;
-    double u_nom = 0.0, u_denom = 0.0;
+double DouglasPeuckerMod::distance_dp(std::vector<double> &p0,
+                                      std::vector<double> &p1,
+                                      std::vector<double> &p2,
+                                      std::list<size_t> &points) {
+  double u,
+         out = 0.0,
+         u_nom = 0.0,
+         u_denom = 0.0;
 
-    if (p1 == p2) {
-      for (list<size_t>::iterator i = points.begin(); i != points.end(); i++) {
-        out += pow(p2[*i] - p0[*i], 2);
+  if (p1 == p2) {
+    for (auto i = points.begin(); i != points.end(); i++) {
+      out += pow(p2[*i] - p0[*i], 2);
+    }
+  } else {
+    for (auto i = points.begin(); i != points.end(); i++) {
+      u_nom += (p0[*i] - p1[*i]) * (p2[*i] - p1[*i]);
+    }
+
+    for (auto i = points.begin(); i != points.end(); i++) {
+      u_denom += pow(p2[*i] - p1[*i], 2);
+    }
+
+    u = u_nom / u_denom;
+
+    if (u <= 0) {
+      for (auto i = points.begin(); i != points.end(); i++) {
+        out += pow(p0[*i] - p1[*i], 2);
       }
-    } else {
-        for (list<size_t>::iterator i = points.begin(); i != points.end(); i++) {
-          u_nom += (p0[*i] - p1[*i]) * (p2[*i] - p1[*i]);
-        }
-        for (list<size_t>::iterator i = points.begin(); i != points.end(); i++) {
-          u_denom += pow(p2[*i] - p1[*i], 2);
-        }
-
-        u = u_nom / u_denom;
-
-        if (u <= 0) {
-          for (list<size_t>::iterator i = points.begin(); i != points.end(); i++) {
-            out += pow(p0[*i] - p1[*i], 2);
-          }
-        } else if (u >= 1) {
-          for (list<size_t>::iterator i = points.begin(); i != points.end(); i++) {
-            out += pow(p0[*i] - p2[*i], 2);
-          }
-        } else if (0 < u && u < 1) {
-          for (list<size_t>::iterator i = points.begin(); i != points.end(); i++) {
-            out += pow(p0[*i] - p1[*i] - u * (p2[*i] - p1[*i]), 2);
-          }
-        }
+    } else if (u >= 1) {
+      for (auto i = points.begin(); i != points.end(); i++) {
+        out += pow(p0[*i] - p2[*i], 2);
+      }
+    } else if (0 < u && u < 1) {
+      for (auto i = points.begin(); i != points.end(); i++) {
+        out += pow(p0[*i] - p1[*i] - u * (p2[*i] - p1[*i]), 2);
+      }
     }
+  }
 
-    return out;
+  return out;
 }
 
-double DouglasPeuckerMod::distance_simple(vector<double>& p0, vector<double>& p1, vector<double>& p2, list<size_t>& points) {
-    double out = 0.0;
+double DouglasPeuckerMod::distance_simple(std::vector<double> &p0,
+                                          std::vector<double> &p1,
+                                          std::vector<double> &p2,
+                                          std::list<size_t> &points) {
+  double out = 0.0;
 
-    for (list<size_t>::iterator i = points.begin(); i != points.end(); i++) {
-      out += sqrt(abs(p1[*i] - p0[*i])) + sqrt(abs(p2[*i] - p0[*i]));
-    }
+  for (auto i = points.begin(); i != points.end(); i++) {
+    out += sqrt(abs(p1[*i] - p0[*i])) + sqrt(abs(p2[*i] - p0[*i]));
+  }
 
-    out = pow(out, 2)/4;
+  out = pow(out, 2)/4;
 
-    return pow(out, 2);
+  return pow(out, 2);
 }
-vector<int> DouglasPeuckerMod::classify(size_t n_points, const double dists[], double absMaxDist) {
-    vector<int> r;
 
-    if (forceEndpoints) {
-        r.push_back(numLevels - 1);
+std::vector<int> DouglasPeuckerMod::classify(size_t n_points,
+                                             const double dists[],
+                                             double abs_max_dist) {
+  std::vector<int> r;
+
+  if (force_endpoints) {
+    r.push_back(numLevels - 1);
+  } else {
+    r.push_back(numLevels - computeLevel(abs_max_dist) - 1);
+  }
+
+  if (n_points > 1) {
+    for (size_t i = 1; i < n_points - 1; i++) {
+      if (dists[i] != 0.0)
+        r.push_back(num_levels - computeLevel(dists[i]) - 1);
+      else
+        r.push_back(-1);
+    }
+
+    if (force_endpoints) {
+      r.push_back(numLevels - 1);
     } else {
-        r.push_back(numLevels - computeLevel(absMaxDist) - 1);
+      r.push_back(numLevels - computeLevel(abs_max_dist) - 1);
     }
+  }
 
-    if (n_points > 1) {
-        for (size_t i=1; i<n_points-1; i++) {
-
-            if (dists[i] != 0.0)
-              r.push_back(numLevels - computeLevel(dists[i]) - 1);
-            else
-              r.push_back(-1);
-        }
-
-        if (forceEndpoints) {
-            r.push_back(numLevels - 1);
-        } else {
-            r.push_back(numLevels - computeLevel(absMaxDist) - 1);
-        }
-    }
-
-    return r;
+  return r;
 }
 
 /**
@@ -173,12 +217,14 @@ vector<int> DouglasPeuckerMod::classify(size_t n_points, const double dists[], d
  * terms of a logarithm, but this approach makes it a bit easier to ensure
  * that the level is not too large.
  */
-int DouglasPeuckerMod::computeLevel(double absMaxDist) {
-    int lev = 0;
-    if (absMaxDist > threshold) {
-        while (absMaxDist < zoomLevelBreaks[lev]) {
-            lev++;
-        }
+int DouglasPeuckerMod::computeLevel(double abs_max_dist) {
+  int lev = 0;
+
+  if (abs_max_dist > threshold) {
+    while (abs_max_dist < zoom_level_breaks[lev]) {
+      lev++;
     }
-    return lev;
+  }
+
+  return lev;
 }
--- skylines/src/DouglasPeuckerMod.hpp
+++ skylines/src/DouglasPeuckerMod.hpp
@@ -1,3 +1,27 @@
+/* Copyright_License {
+
+  XCSoar Glide Computer - http://www.xcsoar.org/
+  Copyright (C) 2000-2013 The XCSoar Project
+  A detailed list of copyright holders can be found in the file "AUTHORS".
+
+  This program is free software; you can redistribute it and/or
+  modify it under the terms of the GNU General Public License
+  as published by the Free Software Foundation; either version 2
+  of the License, or (at your option) any later version.
+
+  This program is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License
+  along with this program; if not, write to the Free Software
+  Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+}
+
+  Original code of Douglas-Peucker algorithm by Robert Coup <robert.coup@koordinates.com>
+*/
+
 #ifndef SKYLINES_DOUGLASPEUCKERMOD_H
 #define SKYLINES_DOUGLASPEUCKERMOD_H
 
@@ -6,26 +30,45 @@
 #include <memory>
 
 class DouglasPeuckerMod {
-
 private:
-    int numLevels;
-    int zoomFactor;
-    double threshold;
-    bool forceEndpoints;
-    double *zoomLevelBreaks;
-
-    double distance_dp(std::vector<double>& p0, std::vector<double>& p1, std::vector<double>& p2, std::list<size_t>& points);
-    double distance_simple(std::vector<double>& p0, std::vector<double>& p1, std::vector<double>& p2, std::list<size_t>& points);
-    int computeLevel(double absMaxDist);
-    std::vector<int> classify(size_t n_points, const double dists[], double absMaxDist);
+  int num_levels;
+  int zoom_factor;
+  double threshold;
+  bool force_endpoints;
+  double *zoom_level_breaks;
 
 public:
-    DouglasPeuckerMod(int numLevels=18, int zoomFactor=2, double threshold=0.00001, bool forceEndpoints=true);
-    ~DouglasPeuckerMod();
+  DouglasPeuckerMod(int _num_levels = 18,
+                    int _zoom_factor = 2,
+                    double _threshold = 0.00001,
+                    bool _force_endpoints = true);
 
-    std::vector<int> dpEncode(std::vector<std::vector<double> >& points, char *type);
-    int getNumLevels() { return numLevels; }
-    int getZoomFactor() { return zoomFactor; }
+  ~DouglasPeuckerMod();
+
+  std::vector<int> dpEncode(std::vector<std::vector<double>> &points, char *type);
+
+  int getNumLevels() {
+    return num_levels;
+  }
+
+  int getZoomFactor() {
+    return zoom_factor;
+  }
+
+private:
+  double distance_dp(std::vector<double> &p0,
+                     std::vector<double> &p1,
+                     std::vector<double> &p2,
+                     std::list<size_t> &points);
+
+  double distance_simple(std::vector<double> &p0,
+                         std::vector<double> &p1,
+                         std::vector<double> &p2,
+                         std::list<size_t> &points);
+
+  int computeLevel(double abs_max_dist);
+
+  std::vector<int> classify(size_t n_points, const double dists[], double abs_max_dist);
 };
 
 #endif /* SKYLINES_DOUGLASPEUCKERMOD_H */
